ecb0c77e2a6a553d161021d11fe5cf916f90147c
nayara <nayara.dias@stations.cloud>
2026-07-21 22:02:28 +0200

    first commit

---
+++ src/main/kotlin/com/mimi/pipeline_project/PipelineProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.mimi.pipeline_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PipelineProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<PipelineProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/mimi/pipeline_project/PipelineProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mimi.pipeline_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PipelineProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
